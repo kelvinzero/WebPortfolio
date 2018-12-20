2bf83bd91d59f9fed6e5af462a9031601786ada2
Josh Cotes <jcotes80@ewu.edu>
2018-12-19 20:05:32 -0700

    initial working commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing/landing-page/landing-page.component';
+import { HeaderComponent } from './pages/components/header/header.component';
+import {MatToolbarModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
