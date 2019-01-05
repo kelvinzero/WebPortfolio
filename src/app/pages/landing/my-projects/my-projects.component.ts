f078c701c7ba36a35be56181bf9464dddde5141a
Josh Cotes <jcotes80@ewu.edu>
2019-01-04 19:23:00 -0700

    commit issues

---
+++ src/app/pages/landing/my-projects/my-projects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-projects',
+  templateUrl: './my-projects.component.html',
+  styleUrls: ['./my-projects.component.css']
+})
+export class MyProjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
